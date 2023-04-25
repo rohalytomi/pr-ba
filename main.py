e35b5c10769287f9b78f4357065ca5a765f39411
rohalytamas <rohaly.tk04@gmail.com>
2023-04-25 11:01:04 +0200

    uj fuggveny

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def osszead(a,b):
-    return a+b
-def kivon(a,b):
-    return a-b
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
+    np.random.seed(seed)
+    y=np.polyval(coefficients[::-1],x)+ np.random.normal(0,noise_std, len(x))
+    return x,y
