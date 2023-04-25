bc6dd7dc8faaafc40dfc7fa8f8ad4b06ae00858c
rohalytamas <rohaly.tk04@gmail.com>
2023-04-25 11:40:27 +0200

    feladat

---
+++ regresszio.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from main import generate_synthetic_data
+
+def fit_linear_regression(x,y):
+    lr=LinearRegression()
+    lr.fit(x.reshape(-1,-1),y)
+    return lr
