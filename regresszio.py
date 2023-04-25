a690e06fcdb7e0635b9fcd3c18dbba3155465de7
rohalytamas <rohaly.tk04@gmail.com>
2023-04-25 11:42:31 +0200

    eleje vege

--- regresszio.py
+++ regresszio.py
@@ -7,3 +7,10 @@ def fit_linear_regression(x,y):
     lr=LinearRegression()
     lr.fit(x.reshape(-1,-1),y)
     return lr
+
+def main():
+    coefficients = [100, 0.02, -0.002, 0.014]
+    x_values = np.linspace(-10, 10, 100)
+    x, y = generate_synthetic_data(x_values, coefficients)
+    lr=fit_linear_regression(x,y)
+    visualize_data_and_fit(x, y, lr)
