b5201b0530b2e18e39707a6d674af81fdc85a140
AlbreMar <martin.albrecht@stun.uni-corvinus.hu>
2023-04-25 11:34:28 +0200

    first done

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
     y=np.polyval(coefficients[::-1],x)+ np.random.normal(0,noise_std, len(x))
     return x,y
 
+
+
 def visualize_data(x, y):
     plt.scatter(x, y)
     plt.xlabel("Feature (x)")
@@ -13,8 +15,8 @@ def visualize_data(x, y):
     plt.title("Synthetic Data with Polynomial Relationship and Noise")
     plt.show()
 
-def main():
-    coefficients = [1, 0.02, -0.002, 0.014]
-    x_values = np.linspace(-10, 10, 100)
-    x, y = generate_synthetic_data(x_values, coefficients)
-    visualize_data(x, y)
+
+coefficients = [1, 0.02, -0.002, 0.014]
+x_values = np.linspace(-10, 10, 100)
+x, y = generate_synthetic_data(x_values, coefficients)
+visualize_data(x, y)
