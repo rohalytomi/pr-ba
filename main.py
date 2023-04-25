a95b283e96e3f0ba86ce91a3cd5dbac213108b62
AlbreMar <martin.albrecht@stun.uni-corvinus.hu>
2023-04-25 10:59:46 +0200

    2nd part

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def osszead(a,b):
-    return a+b
-def kivon(a,b):
-    return a-b
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize_data(x, y):
+    plt.scatter(x, y)
+    plt.xlabel("Feature (x)")
+    plt.ylabel("Target (y)")
+    plt.title("Synthetic Data with Polynomial Relationship and Noise")
+    plt.show()
+
+def main():
+    coefficients = [1, 0.02, -0.002, 0.014]
+    x_values = np.linspace(-10, 10, 100)
+    x, y = generate_synthetic_data(x_values, coefficients)
+    visualize_data(x, y)
